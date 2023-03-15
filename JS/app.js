4c0942688a30c07340d2e674d19c58887cb310c8
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2023-03-15 05:27:48 +0100

    commit

--- JS/app.js
+++ JS/app.js
@@ -2,16 +2,31 @@ $(document).ready(function () {
   // slider
   $("#hero-slider").owlCarousel({
     loop: true,
+    autoplay: true,
+    // autoplayTimeout: 6000,
+    // autoplayHoverPause: true,
     margin: 0,
     nav: true,
     items: 1,
     smartSpeed: 1000,
     navText: ["<<", ">>"],
-    dot: true,
+    dot: false,
     responsive: {
       0: {},
       600: {},
       1000: {},
     },
+  });
+
+  //project slider
+  $("#project-slider").owlCarousel({
+    items: 2,
+    loop: true,
+    nav: true,
+    dots: true,
+    margin: 10,
+    smartSpeed: 1000,
+    // autoplay: true,
+    autoplayTimeout: 5000,
   });
 });
